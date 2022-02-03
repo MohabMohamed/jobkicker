2b27d7e94a7712778dc7abc9962cc602f2fbf1f6
Mohab Abd El-Dayem <mohab.m.mohamed@gmail.com>
2022-02-03 18:35:20 +0200

    Add Job and JobQueue structs

--- job.go
+++ job.go
@@ -1,1 +1,23 @@
 package jobkicker
+
+import "time"
+
+type JobType int8
+
+const (
+	Once JobType = iota
+	periodically
+)
+
+type Job struct {
+	jobType JobType
+	fn      interface{}
+	args    []interface{}
+	timer   *time.Timer
+	ticker  *time.Ticker
+}
+
+type JobQueue struct {
+	pendingJobs map[string]*Job
+	doneJobs    map[string]*time.Time //Done jobs with it's finish time
+}
