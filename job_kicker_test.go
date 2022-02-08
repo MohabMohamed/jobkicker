7bd0f101d84f993a047bd15d94b04e099f243a87
Mohab Abd El-Dayem <mohab.m.mohamed@gmail.com>
2022-02-08 12:01:11 +0200

    Add unittest for KickOnceAfter

---
+++ job_kicker_test.go
@@ -0,0 +1,54 @@
+package jobkicker
+
+import (
+	"testing"
+	"time"
+)
+
+func testingTask1() {
+	println("Hello from job 1")
+}
+
+func TestKickOnceAfter(t *testing.T) {
+	jk := NewScheduler(nil, nil)
+	jk.jobQueue.Lock()
+	pendingJobsSize := len(jk.jobQueue.PendingJobs)
+	doneJobsSize := len(jk.jobQueue.DoneJobs)
+	if pendingJobsSize != 0 {
+		t.Errorf("newly intialized jobkicker's pendingjobs should be empty but it's size %d", pendingJobsSize)
+	}
+	if doneJobsSize != 0 {
+		t.Errorf("newly intialized jobkicker's donejobs should be empty but it's size %d", doneJobsSize)
+	}
+	t.Log("newly intialized jobkicker's jobqueue is empty passed")
+
+	jk.jobQueue.Unlock()
+
+	delay := time.Date(0, 0, 0, 0, 0, 3, 0, time.UTC)
+	jk.KickOnceAfter(delay, testingTask1)
+	jk.jobQueue.Lock()
+	pendingJobsSize = len(jk.jobQueue.PendingJobs)
+	doneJobsSize = len(jk.jobQueue.DoneJobs)
+	if pendingJobsSize != 1 {
+		t.Errorf("jobkicker's pendingjobs should have size %d but found it's size %d", 1, pendingJobsSize)
+	}
+	if doneJobsSize != 0 {
+		t.Errorf("jobkicker's donejobs should have size %d but found it's size %d", 0, doneJobsSize)
+	}
+	jk.jobQueue.Unlock()
+	t.Log("scheduling new job passed")
+
+	time.Sleep(4 * time.Second)
+	jk.jobQueue.Lock()
+	pendingJobsSize = len(jk.jobQueue.PendingJobs)
+	doneJobsSize = len(jk.jobQueue.DoneJobs)
+	if pendingJobsSize != 0 {
+		t.Errorf("jobkicker's pendingjobs should have size %d but found it's size %d", 1, pendingJobsSize)
+	}
+	if doneJobsSize != 1 {
+		t.Errorf("jobkicker's donejobs should have size %d but found it's size %d", 0, doneJobsSize)
+	}
+	jk.jobQueue.Unlock()
+	t.Log("finishing scheduled job passed")
+
+}
